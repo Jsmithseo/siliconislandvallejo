4125510a4f8b49ba4f043086bb25a12974acc36c
Jeremy Smith <prez@Jeremys-Air.lan>
2023-09-09 15:17:28 -0700

    first commit

---
+++ pages/index.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import { Container, Row, Col, Button } from 'reactstrap';
+import Image from 'next/image';
+import Logo from '../public/logo.png';
+import ContactUs from './components/ContactUsform';
+
+export default function Index() {
+  const MainStyle = {
+    overflowX: 'hidden',
+  };
+
+  const companyTitle = {
+    textAlign: 'center',
+    fontFamily: 'avenir',
+  };
+
+  const copy = {
+    textAlign: 'center',
+    fontSize: '30px',
+    fontFamily: 'avenir',
+  };
+  return (
+    <div style={MainStyle}>
+      <center>
+        <Image src={Logo} alt="Description of Image" width={300} height={300} />
+      </center>
+
+      <h2 style={companyTitle}>
+        Onyx Creative Labs: Where Web Development Meets Craftsmanship
+      </h2>
+      <Container>
+        <div style={copy}>
+          Just as a barber college hones the art of the perfect trim and style,
+          Onyx Creative Labs sharpens your skills in the intricate world of web
+          development. Dive into the craftsmanship behind every pixel and line
+          of code, and emerge as a master of the digital domain.
+        </div>
+        <ContactUs />
+      </Container>
+    </div>
+  );
+}
---
+++ pages/sendMail.js
@@ -0,0 +1,31 @@
+import nodemailer from 'nodemailer';
+import { useRouter } from 'next/router';
+
+export default async (req, res) => {
+  if (req.method === 'POST') {
+    const { email, name, message } = req.body;
+
+    const transporter = nodemailer.createTransport({
+      service: 'Gmail',
+      auth: {
+        user: 'Jeremysmithseo@gmail.com',
+        pass: 'rjuabcwfiwqgvsyf',
+      },
+    });
+
+    try {
+      const emailRes = await transporter.sendMail({
+        from: email,
+        to: 'Jeremysmithseo@gmail.com',
+        subject: `New contact from ${email}`,
+        text: `${email} ${name} ${message}`,
+      });
+
+      return res.status(200).send({ done: true });
+    } catch (err) {
+      return res.status(500).send({ error: 'Failed to send email.' });
+    }
+  }
+
+  return res.status(404).send();
+};
