a03ab1f6bddeaea262b936eb1e397048c443ddfe
Jeremy Smith <prez@Jeremys-MacBook-Air.local>
2023-09-09 19:49:38 -0700

    fix form

--- pages/components/ContactUsform.js
+++ pages/components/ContactUsform.js
@@ -3,35 +3,30 @@ import { Button, Form, FormGroup, Label, Input, Container } from 'reactstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 const ContactUs = () => {
-    
+
     const handleSubmit = async (e) => {
         e.preventDefault();
     
         const formData = new FormData(e.target);
         const data = {};
         formData.forEach((value, key) => (data[key] = value));
-
-    const response = await fetch('./api/sendMail.js', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(formData),
-    });
-    if (response.status === 200) {
-      console.log('Email sent successfully');
-      window.location.href = '/thankyou';
-    } else {
-      console.error('Error sending email');
-    }
-
-    setFormData({
-      name: '',
-      email: '',
-      message: '',
-    });
-  };
-
+    
+        const response = await fetch('./api/sendMail', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify(data),
+        });
+    
+        if (response.status === 200) {
+          console.log('Email sent successfully');
+          window.location.href = '/thankyou';
+        } else {
+          console.error('Error sending email');
+        }
+      };
+      
   return (
     <Container>
       <br></br>
