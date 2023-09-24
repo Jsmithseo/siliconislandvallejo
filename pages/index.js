51689ae55c9b795f3aa7fea57f48f20eeda31aaa
SmittyHamm <smittyhamm@Chloes-MacBook-Air.local>
2023-09-23 20:13:44 -0700

    change container to material design

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,15 @@
 import React from 'react';
-import { Container, Row, Col, Button } from 'reactstrap';
 import Image from 'next/image';
 import Logo from '../public/logo.PNG';
 import ContactUs from './components/ContactUsform';
+import { Button } from 'reactstrap';
+import { Button as MuiButton } from '@mui/material';
+import { jsx } from '@emotion/react';
+import styled from '@emotion/styled';
+import CssBaseline from '@mui/material/CssBaseline';
+import Box from '@mui/material/Box';
+import Container from '@mui/material/Container';
+
 
 export default function Index() {
   const MainStyle = {
@@ -20,23 +27,32 @@ export default function Index() {
     fontFamily: 'avenir',
   };
   return (
+    
     <div style={MainStyle}>
+          <React.Fragment>
+      <CssBaseline />
+      <Container maxWidth="lg">
       <center>
-        <Image src={Logo} alt="Description of Image" width={300} height={300} />
+        <Image src={Logo} alt="Description of Image" width={300} height={275} />
       </center>
-
-      <h2 style={companyTitle}>
+        <h2 style={companyTitle}>
         Onyx Creative Labs: Where Web Development Meets Craftsmanship
       </h2>
-      <Container>
         <div style={copy}>
           Just as a barber college hones the art of the perfect trim and style,
           Onyx Creative Labs sharpens your skills in the intricate world of web
           development. Dive into the craftsmanship behind every pixel and line
           of code, and emerge as a master of the digital domain.
         </div>
-        <ContactUs />
       </Container>
+    </React.Fragment>
+
+        <ContactUs />
+            <div>
+      {/* <MuiButton variant="contained" color="primary">
+        Material-UI Button
+      </MuiButton> */}
+    </div>
     </div>
   );
 }
