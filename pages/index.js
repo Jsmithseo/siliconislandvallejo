e25cfc0771833541ca03cabf817ab9ee8afbea46
Jeremy Smith <prez@Jeremys-MacBook-Air.local>
2024-09-03 13:37:41 -0700

    wip

--- pages/index.js
+++ pages/index.js
@@ -5,132 +5,139 @@ import CssBaseline from '@mui/material/CssBaseline';
 import Container from '@mui/material/Container';
 import Link from 'next/link';
 import BaseLayout from './components/layouts/BaseLayouts.js';
-import HowWeWork from './components/HowWework';
-import DonationFormWrapper from './components/DonationForm';
 import { Row, Col, Button } from 'reactstrap';
 
 export default function Index() {
-  const MainStyle = {
-    overflowX: 'hidden',
-  };
-
-  const copy = {
-    textAlign: 'center',
-    fontSize: '22px',
-    fontFamily: 'avenir',
-    margin: '20px 0', // Use margin instead of <br>
-    padding: '0 20px', // Add padding for better spacing on mobile
-  };
-
-  const buttonContainer = {
-    display: 'flex',
-    justifyContent: 'center',
-    gap: '20px', // Space between buttons
-    marginTop: '20px',
+  const styles = {
+    main: {
+      overflowX: 'hidden',
+    },
+    headerBackground: {
+      backgroundColor: '#f0f0f0', 
+      padding: '50px 0',
+      width: '100vw',
+      marginLeft: 'calc(-50vw + 50%)',
+    },
+    backgroundSection: {
+      backgroundColor: '#f0f0f0',
+      padding: '50px 0',
+      width: '100vw', // Ensures the background spans full width
+      marginLeft: 'calc(-50vw + 50%)', // Centers the full-width background
+    },
+    content: {
+      padding: '30px',
+      borderRadius: '8px',
+    },
+    header: {
+      fontSize: '2.5rem',
+      marginBottom: '20px',
+      fontWeight: '400',
+    },
+    text: {
+      fontSize: '1.5rem',
+      marginBottom: '30px',
+      textAlign: 'center',
+      fontFamily: 'avenir',
+    },
+    button: {
+      padding: '10px 20px',
+    },
+    buttonContainer: {
+      display: 'flex',
+      justifyContent: 'center',
+      gap: '20px',
+      marginTop: '20px',
+    },
+    logoContainer: {
+      marginTop: '40px',
+      textAlign: 'center',
+    },
   };
 
   return (
     <BaseLayout>
-      <div style={MainStyle}>
-        <React.Fragment>
-          <CssBaseline />
-          {/* Full-width background for the logo, text, and buttons */}
-          <div style={styles.headerBackground}>
-            <Container>
-              <div style={{ marginTop: '40px', textAlign: 'center' }}>
-                <Image
-                  src={Logo}
-                  alt="Onyx Creative Labs Logo"
-                  width={300}
-                  height={75}
-                />
-              </div>
-              <div style={copy}>
-              We’re a full-service digital marketing and development agency dedicated to delivering innovative technology solutions for small businesses and nonprofits. We specialize in web development, SEO, and content marketing, all while nurturing the next generation of tech talent through hands-on experience and mentorship.
-              </div>
-              <div style={buttonContainer}>
-                <Link href="/contact">
-                  <Button variant="contained" color="success" type="submit">
-                    Start Project
-                  </Button>
-                </Link>
-                <Link href="https://calendly.com/jeremy-onyxcreativelabs/client-discovery">
-                  <Button variant="contained" color="primary" type="submit">
-                    30 Min Free Consult
-                  </Button>
-                </Link>
-              </div>
-            </Container>
-          </div>
-
-          <Container maxWidth="lg">
-            <div style={{ marginTop: '60px' }}>
-              <HowWeWork />
+      <div style={styles.main}>
+        <CssBaseline />
+        <div style={styles.headerBackground}>
+          <Container>
+            <div style={styles.logoContainer}>
+              <Image
+                src={Logo}
+                alt="Onyx Creative Labs Logo"
+                width={300}
+                height={75}
+              />
             </div>
-            <div style={{ marginTop: '60px' }}>
-              <div style={styles.background}>
-                <Container>
-                  <Row>
-                    <Col
-                      md={{ size: 12 }}
-                      className="text-center"
-                      style={styles.content}
-                    >
-                      {/* <h1 style={styles.header}>
-                        Empower the Next Generation of Tech Talent
-                      </h1> */}
-                      <p style={styles.text}>
-                      What sets us apart is our unique approach to talent development. Your projects will be handled by our talented students, who work under the guidance of an experienced senior developer acting as a mentor. This ensures that your website or app is not only expertly crafted but also continually maintained with the latest industry practices.
-                      </p>
-                      <Button
-                        color="success"
-                        size="lg"
-                        style={styles.button}
-                        href="/contact"
-                      >
-                        Start Project
-                      </Button>
-                      <br></br>
-                      <br></br>
-                    </Col>
-                  </Row>
-                </Container>
-              </div>
+            <p style={{ ...styles.text, padding: '0 20px', margin: '20px 0' }}>
+              We’re a full-service digital marketing and development agency dedicated to delivering innovative technology solutions for small businesses and nonprofits. We specialize in web development, SEO, and content marketing, all while nurturing the next generation of tech talent through hands-on experience and mentorship.
+            </p>
+            <div style={styles.buttonContainer}>
+              <Link href="/contact">
+                <Button variant="contained" color="success" type="submit">
+                  Start Project
+                </Button>
+              </Link>
+              <Link href="https://calendly.com/jeremy-onyxcreativelabs/client-discovery">
+                <Button variant="contained" color="primary" type="submit">
+                  30 Min Free Consult
+                </Button>
+              </Link>
             </div>
           </Container>
-        </React.Fragment>
+        </div>
+        <Container maxWidth="lg">
+          <div style={{ marginTop: '60px' }}>
+            <Container>
+              <Row>
+                <Col
+                  md={{ size: 12 }}
+                  className="text-center"
+                  style={styles.content}
+                >
+                  <h1 style={styles.header}>How It Works</h1>
+                  <p style={styles.text}>
+                    At Onyx Creative Labs, we provide high-quality React development services by leveraging the skills of our students who are eager to gain real-world experience. Our unique approach ensures your project is handled with care, expertise, and a fresh perspective.
+                  </p>
+                  <Button
+                    color="success"
+                    size="lg"
+                    style={styles.button}
+                    href="/how-it-works"
+                  >
+                    Learn More
+                  </Button>
+                </Col>
+              </Row>
+            </Container>
+            <div style={styles.backgroundSection}>
+              <Container>
+                <Row>
+                  <Col
+                    md={{ size: 12 }}
+                    className="text-center"
+                    style={styles.content}
+                  >
+                    <h1 style={styles.header}>
+                      Empower the Next Generation of Tech Talent
+                    </h1>
+                    <p style={styles.text}>
+                      What sets us apart is our unique approach to talent development. Your projects will be handled by our talented students, who work under the guidance of an experienced senior developer acting as a mentor. This ensures that your website or app is not only expertly crafted but also continually maintained with the latest industry practices.
+                    </p>
+                    <Button
+                      color="success"
+                      size="lg"
+                      style={styles.button}
+                      href="/contact"
+                    >
+                      Start Project
+                    </Button>
+                  </Col>
+                </Row>
+              </Container>
+            </div>
+          </div>
+        </Container>
       </div>
     </BaseLayout>
   );
 }
-
-const styles = {
-  headerBackground: {
-    backgroundColor: '#f0f0f0', // Light grey background for the header section
-    padding: '50px 0', // Padding to space out the content
-    width: '100vw', // Full width background
-    marginLeft: 'calc(-50vw + 50%)', // Adjust margin to align with the full-width background
-  },
-  background: {
-    backgroundColor: '#f0f0f0', // Light grey background for the section below
-    padding: '50px 0', // Padding to space out the content
-    width: '100vw', // Ensure the background spans the full width of the viewport
-    marginLeft: 'calc(-50vw + 50%)', // Adjust margin to align with the full-width background
-  },
-  content: {
-    padding: '30px', // Padding inside the content area
-    borderRadius: '8px', // Rounded corners
-  },
-  header: {
-    fontSize: '2.5rem',
-    marginBottom: '20px',
-    fontWeight: 'bold',
-  },
-  text: {
-    fontSize: '1.2rem',
-    marginBottom: '30px',
-  },
-  button: {
-    padding: '10px 20px',
-  },
-};
